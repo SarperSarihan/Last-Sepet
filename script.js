90263c5cd5f946ec4c8a55dc160b4acf32724dd9
SarperSarihan <sarihan.sarper@hotmail.com>
2023-11-24 13:29:11 +0300

    toplam fiyat ve satın al butonu

--- script.js
+++ script.js
@@ -1,191 +1,312 @@
-// const row=document.querySelector('.row');
-const span=document.querySelector('#span')
+const span = document.querySelector("#span");
+// const row = document.querySelector('.row')
 
+let sepet = [];
 
-let sepet=[
+let localItem = localStorage.getItem("sepet");
 
-]
+let toplamSepet = 0;
 
-let localItem=localStorage.getItem('sepet')
 if (localItem) {
-    sepet=JSON.parse(localItem)
-    console.log(sepet)
-    span.textContent=sepet.length
-    
-
+    sepet = JSON.parse(localItem);
+    sepet.forEach((urun) => {
+        toplamSepet += urun.quantity;
+    });
+    if (sepet.length == 0) {
+        span.textContent = " ";
+    } else {
+        span.textContent = toplamSepet;
+    }
 }
+// localStorage.clear();
 
-console.log(window.location.href);
-console.log(sepet.length);
+if (window.location.href == "http://127.0.0.1:5500/index.html") {
+    const row = document.querySelector(".row");
 
-if(window.location.href=='http://127.0.0.1:5500/index.html'){
-    const row=document.querySelector('.row')
+    //!Arama kısmını yapıcaz
+    const input = document.getElementById("input");
+    input.addEventListener("input", (element) => {
+        let kullaniciDeger = element.target.value.toLowerCase();
+        const col = document.querySelectorAll(".col-12");
 
-    
-urunler.forEach((urun)=>{
-    urun.quantity=1
+        for (let i = 0; i < col.length; i++) {
+            let urunAdi =
+                col[
+                    i
+                ].firstChild.firstChild.nextSibling.firstChild.textContent.toLowerCase();
 
-    const col=document.createElement('div')
-    col.classList.add('col-lg-3', 'col-sm-6' , 'col-12')
+            let urunAciklama =
+                col[
+                    i
+                ].firstChild.firstChild.nextSibling.firstChild.nextSibling.textContent.toLowerCase();
 
-    const parentDiv=document.createElement('div')
-    parentDiv.style.widht='100%'
-    parentDiv.style.height='300px'
-    parentDiv.style.border='1px solid black'
+            if (
+                urunAdi.indexOf(kullaniciDeger) != -1 ||
+                urunAciklama.indexOf(kullaniciDeger) != -1
+            ) {
+                col[i].style.display = "flex";
+            } else {
+                col[i].style.display = "none";
+            }
+        }
+    });
 
+    urunler.forEach((urun) => {
+        // console.log(urun)
 
-    const imgDiv=document.createElement('div')
-    imgDiv.style.width='100%'
-    imgDiv.style.height='150px'
+        urun.quantity = 1;
 
-    const img=document.createElement('img')
-    img.src=urun.fotoğraf
-    img.style.width='100%'
-    img.style.height='100%'
+        // console.log(urun)
 
-    const cardBody=document.createElement('div')
-    cardBody.style.width='100%'
-    cardBody.style.height='120px'
+        const col = document.createElement("div");
+        col.classList.add("col-lg-3", "col-sm-6", "col-12");
 
-    const baslik=document.createElement('h5')
-    baslik.textContent=urun.isim
+        const parentDiv = document.createElement("div");
+        parentDiv.style.width = "100%";
+        parentDiv.style.height = "420px";
+        parentDiv.style.border = "1px solid black";
 
-    const aciklama=document.createElement('p')
-    aciklama.textContent=`${urun.açıklama} - ${urun.fiyat}$`
+        const imgDiv = document.createElement("div");
+        imgDiv.style.width = "100%";
+        imgDiv.style.height = "200px";
 
+        const img = document.createElement("img");
+        img.src = urun.fotoğraf;
+        img.style.width = "100%";
+        img.style.height = "100%";
 
-    // BUTON
-    const btn=document.createElement('button')
-    btn.classList.add('btn' , 'btn-dark')
-    btn.textContent='sepete ekle'
-    btn.addEventListener('click',()=>{
-        // sepet.push(urun);
-        console.log(sepet);
-        sepet.forEach(sepetUrunu=>{
-            onsole.log(sepetUrunu.isim==urun.isim);
-            if (sepetUrunu.isim==urun.isim){
-                sepetUrunu.quantity++
-                
-            }else{
+        const cardBody = document.createElement("div");
+        cardBody.style.width = "100%";
+        cardBody.style.height = "220px";
+
+        const baslik = document.createElement("h5");
+        baslik.textContent = urun.isim;
+
+        const aciklama = document.createElement("p");
+        aciklama.textContent = `${urun.açıklama} - ${urun.fiyat}$`;
+
+        const btn = document.createElement("button");
+        btn.classList.add("btn", "btn-dark");
+        btn.textContent = "Sepete Ekle";
+
+        btn.addEventListener("click", () => {
+            console.log(sepet.length);
+
+            let found = false;
+
+            if (sepet.length == 0) {
+                sepet.push(urun);
+
+                found = true;
+            } else {
+                sepet.forEach((sepetUrunu) => {
+                    // console.log(sepetUrunu.isim == urun.isim)
+                    if (sepetUrunu.isim == urun.isim) {
+                        sepetUrunu.quantity++;
+                        found = true;
+                    }
+                });
+            }
+
+            if (!found) {
                 sepet.push(urun);
             }
 
-        })
-        
-        
-        localStorage.setItem('sepet',JSON.stringify(sepet))
-        span.textContent=sepet.length
+            let toplam = 0;
+            sepet.forEach((element) => {
+                toplam += element.quantity;
+            });
+
+            localStorage.setItem("sepet", JSON.stringify(sepet));
+            span.textContent = toplam;
+        });
+
+        cardBody.append(baslik);
+        cardBody.append(aciklama);
+        cardBody.append(btn);
+
+        imgDiv.append(img);
+
+        parentDiv.append(imgDiv);
+        parentDiv.append(cardBody);
+
+        col.append(parentDiv);
+
+        row.append(col);
     });
+} else if (window.location.href == "http://127.0.0.1:5500/sepet.html"){
+    const container = document.querySelector(".container");
 
-    cardBody.append(baslik)
-    cardBody.append(aciklama)
-    cardBody.append(btn)
+    if (sepet.length == 0) {
+        const h4 = document.createElement("h4");
+        h4.textContent = "Sepetinizde Ürün Yok...";
 
-    imgDiv.append(img)
+        container.append(h4);
+    } else {
+        //! Sepetimin uzunluğu 0 dan büyük olduğu sürece burdaki kodlar çalışacak
+        sepet.forEach((urun) => {
+            const div = document.createElement("div");
+            div.style.width = "100%";
+            div.style.height = "200px";
+            div.style.border = "1px solid black";
+            div.classList.add(
+                "d-flex",
+                "align-items-center",
+                "justify-content-between",
+                "mt-2"
+            );
 
-    parentDiv.append(imgDiv)
-    parentDiv.append(cardBody)
+            const imgDiv = document.createElement("div");
+            imgDiv.style.width = "25%";
+            imgDiv.style.height = "100%";
 
-    col.append(parentDiv)
-    row.append(col)
+            const img = document.createElement("img");
+            img.src = urun.fotoğraf;
+            img.style.width = "100%";
+            img.style.height = "100%";
 
-})  
+            const baslik = document.createElement("h3");
+            baslik.textContent = urun.isim;
 
-}else if (window.location.href == 'http://127.0.0.1:5500/sepet.html') {
+            const price = document.createElement("p");
+            let urununFiyati = urun.fiyat * urun.quantity;
+            price.textContent = urununFiyati.toFixed(2) + "$";
+            price.style.fontWeight = "bold";
 
-    const container=document.querySelector('.container')
-    if (sepet.length==0) {
-        const h4=document.createElement('h4')
-        h4.textContent='Sepetinizde Ürün Yok'
+            const kacTane = document.createElement("div");
+            //? bir şeye ihtiyaç olursa ekleriz
+            kacTane.classList.add("d-flex", "gap-3");
 
-        container.append(h4)
-        
-    }else{
-        //!SEPETİN BÜYÜKLÜĞÜ SIFIRDAN BÜYÜK OLDUĞUNDA ÇALIŞACAK 
-        sepet.forEach(urun=>{
-            const div =document.createElement('div')
-            div.style.width='100%'
-            div.style.height='200px'
-            div.style.border='1px solid black'
-            div.classList.add('d-flex' ,'align-items-center' , 'justify-content-between','mt-2')
+            const sayi = document.createElement("p");
+            sayi.textContent = urun.quantity;
+            sayi.classList.add("mt-3", "fs-4");
 
-            const imgDiv=document.createElement('div')
-            imgDiv.style.width='25%'
-            imgDiv.style.height='100%'
+            const azaltBtn = document.createElement("button");
+            azaltBtn.textContent = "-";
+            azaltBtn.classList.add("btn", "btn-light");
 
-            const img=document.createElement('img')
-            img.src=urun.fotoğraf
-            img.style.width="100%"
-            img.style.height="100%"
+            const arttirBtn = document.createElement("button");
+            arttirBtn.textContent = "+";
+            arttirBtn.classList.add("btn", "btn-light");
 
-            const baslik=document.createElement('h3')
-            baslik.textContent=urun.isim
-            
-            const price=document.createElement('p')
-            price.textContent=urun.fiyat + '$'
-            price.style.fontWeight='bold'
+            //* azalt butonu başlangıcı
 
-            const kacTane=document.createElement("div")
-            kacTane.classList.add('d-flex', 'gap-3')
-            //?bir şeye ihtiyaç olursa ekleriz
+            azaltBtn.addEventListener("click", function () {
+                if (urun.quantity >= 1) {
+                    urun.quantity--;
+                    sayi.textContent = urun.quantity;
+                    price.textContent = (urun.fiyat * urun.quantity).toFixed(2) + "$";
 
-            const sayi=document.createElement('p')
-            sayi.textContent=urun.quantity
-            sayi.classList.add('mt-3','fs-3')
+                    localStorage.setItem("sepet", JSON.stringify(sepet));
 
-            const azaltBtn=document.createElement('button')
-            azaltBtn.textContent='-'
-            azaltBtn.classList.add('btn', 'btn-light')
+                    if (urun.quantity == 0) {
+                        // console.log(this.parentElement.parentElement)
+                        this.parentElement.parentElement.remove();
 
+                        //! LocalStorage'dan silme
+                        let urunIndex = sepet.indexOf(urun);
 
-            const arttirBtn=document.createElement('button')
-            arttirBtn.textContent='+'
-            arttirBtn.classList.add('btn', 'btn-light')
+                        sepet.splice(urunIndex, 1);
 
-            azaltBtn.addEventListener('click',function(){
-                
-                if(urun.quantity>=1){
-                    urun.quantity--
-                    sayi.textContent=urun.quantity
-
-                        if(urun.quantity==0){
-                            this.parentElement.parentElement.remove();
-
-                            let urunIndex=sepet.indexOf(urun)
-                            sepet.splice(urunIndex,1)
-
-                            localStorage.setItem('sepet', JSON.stringify(sepet))
-                        }
+                        localStorage.setItem("sepet", JSON.stringify(sepet));
+                        //! LocalStorage'dan silme
+                    }
+                    let toplam = 0;
+                    sepet.forEach((e) => {
+                        toplam += e.quantity;
+                    });
+                    if (sepet.length == 0) {
+                        span.textContent = " ";
+                    } else {
+                        span.textContent = toplam;
+                    }
                 }
+                if (sepet.length <0) {
+                    const h4 = document.createElement("h4");
+                    h4.textContent = "Sepetinizde Ürün Yok...";
+
+                    container.append(h4);
+                    container.removeChild('hr')
+                }
+                let toplamSepetFiyati=0
+                sepet.forEach(e=>{
+                    toplamSepetFiyati+=e.fiyat*e.quantity
+                })
+
+                sepetFiyat.textContent=toplamSepetFiyati.toFixed(2)
             });
 
-            arttirBtn.addEventListener('click', ()=>{
-                urun.quantity++
-                sayi.textContent=urun.quantity
+            //* azalt butonu bitişi
+
+            /////////////////////////////////////////////////
+
+            //* arttır butonu başlangıcı
+            arttirBtn.addEventListener("click", () => {
+                urun.quantity++;
+                sayi.textContent = urun.quantity;
+                price.textContent = (urun.fiyat * urun.quantity).toFixed(2) + "$";
+
+                localStorage.setItem("sepet", JSON.stringify(sepet));
+                let toplam = 0;
+                sepet.forEach((e) => {
+                    toplam += e.quantity;
+                });
+                span.textContent = toplam;
+
+                // urun.fiyat += urun.quantity
+                // price.textContent = urun.fiyat
+                let toplamSepetFiyati=0
+
+                sepet.forEach(e=>{
+                    toplamSepetFiyati+=e.fiyat*e.quantity
+                })
+
+                sepetFiyat.textContent=toplamSepetFiyati.toFixed(2)
             });
 
-            imgDiv.append(img)
+            //*arttır butonu bitişi
 
-            kacTane.append(azaltBtn)
-            kacTane.append(sayi)
-            kacTane.append(arttirBtn)
+            imgDiv.append(img);
 
-            div.append(imgDiv)
-            div.append(baslik)
-            div.append(price)
-            div.append(kacTane)
+            kacTane.append(azaltBtn);
+            kacTane.append(sayi);
+            kacTane.append(arttirBtn);
 
-            container.append(div)
+            div.append(imgDiv);
+            div.append(baslik);
+            div.append(price);
+            div.append(kacTane);
+
+            container.append(div);
 
             
+                
 
+        });
+        const hr=document.createElement('hr')
+            container.append(hr)
+            
 
-        })
+            const fiyatDiv=document.createElement('div')
+            fiyatDiv.classList.add('w100','d-flex','justify-content-around')
+
+            let sepetFiyat=document.createElement('h3')
+            let toplamSepetFiyati= 0
+
+                sepet.forEach(e=>{
+                    toplamSepetFiyati+=e.fiyat*e.quantity
+                })
+
+                sepetFiyat.textContent=toplamSepetFiyati.toFixed(2)
+
+                const sepetBtn=document.createElement('button')
+                sepetBtn.classList.add('btn','btn-success','w-50')
+                sepetBtn.textContent='Satın Al'
+
+                fiyatDiv.append(sepetFiyat)
+                fiyatDiv.append(sepetBtn)
+                container.append(fiyatDiv)
+
     }
-
-
-    
 }
-
-
-
+// localStorage.clear()
